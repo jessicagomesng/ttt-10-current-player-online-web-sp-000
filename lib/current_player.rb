93f7d408482bd1052a09804ffc43a281792bea6e
Jessica Gomes-Ng <jessicagomesng@gmail.com>
2020-03-22 14:24:58 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -12,9 +12,9 @@ end
 
 
 def current_player(board)
-  if turn_count(board) % 2 != 0
-    "O"
-  else turn_count(board) % 2
+  if turn_count(board) % 2
     "X"
-  end
+  else turn_count(board) % 2 != 0
+  "O"
+end
 end
